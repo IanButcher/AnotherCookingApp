4a25aefc9c8a052cb39181742310940e28955cfd
Luna <ian@pop-os.localdomain>
2023-11-04 23:04:59 -0300

    Deleted prueba.html + JS trys to add recipes

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,25 @@
-// Definimos un array con colores pastel
-var colores = ["#FFD700", "#FFA07A", "#87CEEB", "#98FB98"];
-var indiceColor = 0;
-
-// Función para cambiar el color de fondo y del botón
-function cambiarColor() {
-    document.body.style.backgroundColor = colores[indiceColor];
-    document.getElementById("cambiar-color").style.backgroundColor = colores[indiceColor];
-    indiceColor = (indiceColor + 1) % colores.length;
+// Pastel colours Array
+var colours = ["#FFD700", "#FFA07A", "#87CEEB", "#98FB98"]
+var indiceColor = 0
+// Function to change background
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = colours[indiceColor]
+    document.getElementById("cambiar-color").style.backgroundColor = colours[indiceColor]
+    indiceColor = (indiceColor + 1) % colores.length
 }
+// Run of the function once clicked the button
+document.getElementById("change-colour").addEventListener("click", changeBackgroundColor())
 
-// Agregamos un event listener al botón
-document.getElementById("cambiar-color").addEventListener("click", cambiarColor());
+
+// New Recipe
+const newRecipeInput = document.querySelector('input')
+const newRecipesForm = document.querySelector('form')
+const recipeHolder = document.querySelector('section')
+
+newRecipesForm.addEventListener('submit', (a)=>{
+    a.preventDefault()
+    const newLi = document.createElement('li')
+    newLi.innerText = newRecipeInput.value
+    recipeHolder.append(newLi)
+    newRecipeInput.value = ""
+})
